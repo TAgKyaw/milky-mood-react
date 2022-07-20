f71e7c432ebe955e09ba88b1c618abd293a14901
Than Aung Kyaw <dannynaing727@gmail.com>
2022-07-20 21:55:24 +0630

    Setting up Project Installed dependencies, created Header styled-component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./styles.css";
+import React from "react";
+import { GlobalStyle } from "./styles";
+import HeaderComponent from "./Header";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <GlobalStyle />
+      <HeaderComponent />
+      <h1>This is react</h1>
     </div>
   );
-}
+};
+export default App;
