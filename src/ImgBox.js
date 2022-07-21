778b3aedb281d19877b7dda0fb47234c1d17be3f
Than Aung Kyaw <dannynaing727@gmail.com>
2022-07-21 16:53:22 +0630

    Animating with transform and useEffect Creating easing functions

--- src/ImgBox.js
+++ src/ImgBox.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const Image = styled.div`
+const Image = styled.div.attrs(({ $movedX, $movedY }) => ({
+  style: {
+    transform: `translate(${$movedX}px, ${$movedY}px)`
+  }
+}))`
   background-image: url("/image1.jpeg");
-  background-size: 400px 600px;
   height: 100px;
   width: 100px;
   position: relative;
   background-position: ${({ $initialX, $initialY }) =>
     `-${$initialX * 100}px -${$initialY * 100}px`};
+  background-size: 400px 600px;
 `;
 
-const ImgBox = ({ x, y }) => {
-  return <Image $initialX={x} $initialY={y} />;
+const ImgBox = ({ x, y, percent }) => {
+  const [position, setPosition] = useState([0, 0]);
+
+  useEffect(() => {
+    //schdules events to trigger when dependency values change, will also render after every first render
+    setPosition([
+      Math.random() * 400 - x * 100 - 50,
+      Math.random() * 600 - y * 100 - 50
+    ]);
+  }, [x, y]);
+
+  console.log(position);
+  return (
+    <Image
+      $initialX={x}
+      $initialY={y}
+      $movedX={position[0] * percent}
+      $movedY={position[1] * percent}
+    />
+  );
 };
 
 export default ImgBox;
