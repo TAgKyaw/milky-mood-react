c7680eac932b8ca8b69892c5d41cf56a41fe713e
Than Aung Kyaw <dannynaing727@gmail.com>
2022-07-23 15:22:56 +0630

    Refactoring Form code with Formik useField

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,18 @@
 import React from "react";
 import * as Yup from "yup";
-import { Formik, Form, Field } from "formik";
+// import { Field } from "formik";
+import { useField, Formik, Form } from "formik";
+
+const InputComponent = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <label>
+      {label}: {meta.touched && meta.error && <div>{meta.error}</div>}
+      <input {...field} {...props} />
+    </label>
+  );
+};
+
 const FormComponent = ({ handleSuccess }) => {
   const schema = Yup.object().shape({
     name: Yup.string().required("Required field"),
@@ -16,24 +28,36 @@ const FormComponent = ({ handleSuccess }) => {
     >
       {({ errors, touched }) => (
         <Form>
-          <label>Name:</label>
+          <InputComponent
+            name="name"
+            type="text"
+            label="Name"
+            autoComplete="off"
+          />
+          <InputComponent
+            name="email"
+            type="email"
+            label="Email"
+            autoComplete="off"
+          />
+        </Form>
+      )}
+    </Formik>
+  );
+};
+/* {<label>Name:</label>
           <Field name="name" type="text" autoComplete="off" />
           {touched.name && errors && <div>{errors.name}</div>}
           <label>Email address:</label>
           <Field name="email" type="text" autoComplete="off" />
           {touched.errors && errors && <div>{errors.email}</div>}
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
-    // <form onSubmit={handleSuccess}>
-    //   <label for="name">Name:</label>
-    //   <input name="name" type="text" required autoComplete="off" />
-    //   <label for="email">Email address:</label>
-    //   <input name="email" type="email" required autoComplete="off" />
-    //   <button type="submit">Submit</button>
-    // </form>
-  );
-};
+          <button type="submit">Submit</button>} */
+// <form onSubmit={handleSuccess}>
+//   <label for="name">Name:</label>
+//   <input name="name" type="text" required autoComplete="off" />
+//   <label for="email">Email address:</label>
+//   <input name="email" type="email" required autoComplete="off" />
+//   <button type="submit">Submit</button>
+// </form>
 
 export default FormComponent;
